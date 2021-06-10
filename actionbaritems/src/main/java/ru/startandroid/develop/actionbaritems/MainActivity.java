71eac1fbbe78d769b2064a0bee2285c32acc8438
leil25 <nuriaxmetova93@mail.ru>
2021-06-10 09:35:38 +0300

    Изучить ActionBar
    Навигация - Табы

--- actionbaritems/src/main/java/ru/startandroid/develop/actionbaritems/MainActivity.java
+++ actionbaritems/src/main/java/ru/startandroid/develop/actionbaritems/MainActivity.java
@@ -1,29 +1,65 @@
 package ru.startandroid.develop.actionbaritems;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBar.Tab;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
 
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
+
+    final  String LOG_TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setDisplayShowTitleEnabled(false); // убирает название с ActionBar
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        Tab tab = actionBar.newTab();
+        tab.setText("tab1");
+        tab.setTabListener(this);
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab();
+        tab.setText("tab2");
+        tab.setTabListener(this);
+        actionBar.addTab(tab);
+
+        //getSupportActionBar().setDisplayShowTitleEnabled(false); // убирает название с ActionBar
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }*/
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        Log.d(LOG_TAG, "selected tab: "+ tab.getText());
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+        Log.d(LOG_TAG, "unselected tab: "+ tab.getText());
+    }
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+        Log.d(LOG_TAG, "reselected tab: "+ tab.getText());
     }
 }
