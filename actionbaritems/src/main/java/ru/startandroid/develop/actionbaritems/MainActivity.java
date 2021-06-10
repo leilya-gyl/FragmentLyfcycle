ef0d3b0b7fe3bb10c458305ea1aadb8ea936bbaa
leil25 <nuriaxmetova93@mail.ru>
2021-06-10 09:14:27 +0300

    Изучить ActionBar

---
+++ actionbaritems/src/main/java/ru/startandroid/develop/actionbaritems/MainActivity.java
@@ -0,0 +1,29 @@
+package ru.startandroid.develop.actionbaritems;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // убирает название с ActionBar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
