76b0ba507c41999921077d193cb427ca233f86ee
leil25 <nuriaxmetova93@mail.ru>
2021-06-11 10:18:23 +0300

    Изучить ActionMode: вызов по нажатию на кнопку

--- actionmodeexample/src/main/java/ru/startandroid/develop/actionmodeexample/MainActivity.java
+++ actionmodeexample/src/main/java/ru/startandroid/develop/actionmodeexample/MainActivity.java
@@ -3,12 +3,56 @@ package ru.startandroid.develop.actionmodeexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionMode actionMode;
+    final String LOG_TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    public void onClick(View v) {
+        if(actionMode == null)
+            actionMode = startActionMode(callback);
+        else
+            actionMode.finish();
+    }
+    private ActionMode.Callback callback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.getMenuInflater().inflate(R.menu.context, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            Log.d(LOG_TAG, " Item " + item.getTitle());
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            Log.d(LOG_TAG, " destroy ");
+            actionMode = null;
+        }
+    };
 }
